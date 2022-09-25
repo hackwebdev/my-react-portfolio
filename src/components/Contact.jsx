404e3b83bc18a23b357513a9325d6ecb958dc854
rjaybernardo <raphaeljaybernardo@gmail.com>
2022-09-26 02:34:52 +0800

    align contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,44 +4,48 @@ const Contact = () => {
   return (
     <div
       name='contact'
-      className='w-full h-screen bg-[#0a192f] flex justify-center items-center pt-24 p-4'
+      className='w-full md:h-screen text-gray-300 bg-[#0a192f]  pt-20'
     >
-      <form
-        method='POST'
-        action='https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c'
-        className='flex flex-col max-w-[600px] w-full'
-      >
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-          <p className='text-5xl font-bold inline border-b-4 border-pink-600 text-gray-300'>
+          <p className='text-5xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
             Contact
           </p>
-          <p className='text-pink-600 pt-8 text-center'>
-            Submit the form below or shoot me an email -<br />
-            raphaeljaybernardo@gmail.com
-          </p>
         </div>
-        <input
-          className='bg-[#ccd6f6] p-2'
-          type='text'
-          placeholder='Name'
-          name='name'
-        />
-        <input
-          className='my-4 p-2 bg-[#ccd6f6]'
-          type='email'
-          placeholder='Email'
-          name='email'
-        />
-        <textarea
-          className='bg-[#ccd6f6] p-2'
-          name='message'
-          rows='10'
-          placeholder='Message'
-        ></textarea>
-        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>
-          Let's Collaborate
-        </button>
-      </form>
+        <div className=' flex justify-center items-center'>
+          <form
+            method='POST'
+            action='https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c'
+            className='flex flex-col max-w-[600px] w-full'
+          >
+            <p className='py-6  text-pink-600'>
+              Submit the form below or shoot me an email
+              <br />- raphaeljaybernardo@gmail.com
+            </p>
+            <input
+              className='bg-[#ccd6f6] p-2'
+              type='text'
+              placeholder='Name'
+              name='name'
+            />
+            <input
+              className='my-4 p-2 bg-[#ccd6f6]'
+              type='email'
+              placeholder='Email'
+              name='email'
+            />
+            <textarea
+              className='bg-[#ccd6f6] p-2'
+              name='message'
+              rows='10'
+              placeholder='Message'
+            ></textarea>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>
+              Let's Collaborate
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -24,7 +24,7 @@ const Experience = () => {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${WorkImg})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            className='shadow-lg  group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
